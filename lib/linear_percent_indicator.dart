dbf0222556019426611c2661171f7625f2dbb1fe
Martin Kong <martinkongkh@gmail.com>
2021-10-07 15:47:18 +0100

    Add border radius to the linear progress bar (#127)
    * Add border radius to the linear progress bar
    
    In the case, the `llinearStrokeCap` parameter will be deprecated.
    
    Uses path.addRRect() instead of drawLine() to draw the progress bar
    
    Allows user to control the border radius of the progress bar.
    The progress bar will always have the same ends at the moment, will improve that in the future.
    
    The basic functionality are all there, will refactor some code after a while.
    
    * changes on linear percentage indicator
    
    Added Samples to emphasise the use of the `barRadius` parameter
    
    Deleted code that associated only with  `linearStrokeCap`
    
    Added @Deprecated annotation  to `linearStrokeCap`

--- lib/linear_percent_indicator.dart
+++ lib/linear_percent_indicator.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-enum LinearStrokeCap { butt, round, roundAll }
+@Deprecated('This property is no longer used, please use `barRadius`  instead.')
+enum LinearStrokeCap {
+  butt, round, roundAll }
 
 // ignore: must_be_immutable
 class LinearPercentIndicator extends StatefulWidget {
@@ -41,7 +43,11 @@ class LinearPercentIndicator extends StatefulWidget {
   final Widget? center;
 
   ///The kind of finish to place on the end of lines drawn, values supported: butt, round, roundAll
+  @Deprecated('This property is no longer used, please use barRadius  instead.')
   final LinearStrokeCap? linearStrokeCap;
+
+  /// The border radius of the progress bar (Will replace linearStrokeCap)
+  final Radius? barRadius; 
 
   ///alignment of the Row (leading-widget-center-trailing)
   final MainAxisAlignment alignment;
@@ -101,7 +107,9 @@ class LinearPercentIndicator extends StatefulWidget {
     this.trailing,
     this.center,
     this.addAutomaticKeepAlive = true,
+
     this.linearStrokeCap,
+    this.barRadius,
     this.padding = const EdgeInsets.symmetric(horizontal: 10.0),
     this.alignment = MainAxisAlignment.start,
     this.maskFilter,
@@ -253,10 +261,11 @@ class _LinearPercentIndicatorState extends State<LinearPercentIndicator>
               progressColor: widget.progressColor,
               linearGradient: widget.linearGradient,
               backgroundColor: widget.backgroundColor,
+              barRadius: widget.barRadius?? Radius.zero, // If radius is not defined, set it to zero
               linearGradientBackgroundColor:
                   widget.linearGradientBackgroundColor,
               linearStrokeCap: widget.linearStrokeCap,
-              lineWidth: widget.lineHeight,
+              lineWidth: widget.lineHeight, // lineWidth is also no longer used
               maskFilter: widget.maskFilter,
               clipLinearGradient: widget.clipLinearGradient,
             ),
@@ -319,7 +328,9 @@ class LinearPainter extends CustomPainter {
   final bool isRTL;
   final Color progressColor;
   final Color backgroundColor;
+  @Deprecated('This property is no longer used, please use `barRadius`  instead.')
   final LinearStrokeCap? linearStrokeCap;
+  final Radius barRadius;
   final LinearGradient? linearGradient;
   final LinearGradient? linearGradientBackgroundColor;
   final MaskFilter? maskFilter;
@@ -331,6 +342,7 @@ class LinearPainter extends CustomPainter {
     required this.isRTL,
     required this.progressColor,
     required this.backgroundColor,
+    required this.barRadius,
     this.linearStrokeCap = LinearStrokeCap.butt,
     this.linearGradient,
     this.maskFilter,
@@ -338,34 +350,25 @@ class LinearPainter extends CustomPainter {
     this.linearGradientBackgroundColor,
   }) {
     _paintBackground.color = backgroundColor;
-    _paintBackground.style = PaintingStyle.stroke;
-    _paintBackground.strokeWidth = lineWidth;
 
     _paintLine.color = progress.toString() == "0.0"
         ? progressColor.withOpacity(0.0)
         : progressColor;
-    _paintLine.style = PaintingStyle.stroke;
-    _paintLine.strokeWidth = lineWidth;
-
-    if (linearStrokeCap == LinearStrokeCap.round) {
-      _paintLine.strokeCap = StrokeCap.round;
-    } else if (linearStrokeCap == LinearStrokeCap.butt) {
-      _paintLine.strokeCap = StrokeCap.butt;
-    } else {
-      _paintLine.strokeCap = StrokeCap.round;
-      _paintBackground.strokeCap = StrokeCap.round;
-    }
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    final start = Offset(0.0, size.height / 2);
-    final end = Offset(size.width, size.height / 2);
-    canvas.drawLine(start, end, _paintBackground);
+    // Draw background first
+    Path backgroundPath = Path();
+    backgroundPath.addRRect(
+      RRect.fromRectAndRadius(Rect.fromLTWH(0, 0, size.width , size.height ), barRadius)
+    );
+    canvas.drawPath(backgroundPath, _paintBackground);
 
     if (maskFilter != null) {
       _paintLine.maskFilter = maskFilter;
     }
+
     if (linearGradientBackgroundColor != null) {
       Offset shaderEndPoint =
           clipLinearGradient ? Offset.zero : Offset(size.width, size.height);
@@ -373,19 +376,26 @@ class LinearPainter extends CustomPainter {
           ?.createShader(Rect.fromPoints(Offset.zero, shaderEndPoint));
     }
 
+    // Then draw progress line
+    final xProgress = size.width * progress;
+    Path linePath = Path();
     if (isRTL) {
-      final xProgress = size.width - size.width * progress;
       if (linearGradient != null) {
         _paintLine.shader = _createGradientShaderRightToLeft(size, xProgress);
       }
-      canvas.drawLine(end, Offset(xProgress, size.height / 2), _paintLine);
+      linePath.addRRect(
+      RRect.fromRectAndRadius(Rect.fromLTWH(size.width -size.width * progress , 0, xProgress, size.height ), barRadius)
+    );
     } else {
-      final xProgress = size.width * progress;
+
       if (linearGradient != null) {
         _paintLine.shader = _createGradientShaderLeftToRight(size, xProgress);
       }
-      canvas.drawLine(start, Offset(xProgress, size.height / 2), _paintLine);
+      linePath.addRRect(
+      RRect.fromRectAndRadius(Rect.fromLTWH(0, 0, xProgress, size.height ), barRadius)
+      );
     }
+    canvas.drawPath(linePath, _paintLine);
   }
 
   Shader _createGradientShaderRightToLeft(Size size, double xProgress) {

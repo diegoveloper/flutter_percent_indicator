00b5c4729ef294fbb1bd58e800b7fae29870ed64
Abdul Manaf P Fassal <84009194+abdulmanafpfassal@users.noreply.github.com>
2022-07-27 10:17:47 +0530

    Update linear_percent_indicator.dart
    the package put the null aware operator on the WidgetsBinding instead of the instance.
    changed from WidgetsBinding.instance.addPostFrameCallback((_) { to WidgetsBinding.instance?.addPostFrameCallback((_) {

--- lib/linear_percent_indicator.dart
+++ lib/linear_percent_indicator.dart
@@ -159,7 +159,7 @@ class _LinearPercentIndicatorState extends State<LinearPercentIndicator>
 
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
       if (mounted) {
         setState(() {
           _containerWidth = _containerKey.currentContext?.size?.width ?? 0.0;
